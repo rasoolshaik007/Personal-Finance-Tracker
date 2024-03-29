1b791ff37f8aecccbfecd2d48d36b666df24e0ec
VinayJyothi <105520754+VinayJyothi@users.noreply.github.com>
2024-03-28 21:44:20 -0500

    user

---
+++ java/Data/User.java
@@ -0,0 +1,65 @@
+package edu.nwmissouri.personalfinancetracker.data;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "user")
+public class User {
+
+    @PrimaryKey
+    @NonNull
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String mobile;
+
+    public User(String email, String firstName, String lastName, String password, String mobile) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+}
